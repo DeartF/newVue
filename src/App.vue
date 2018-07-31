561f4b0726c29bae3b12b657dd846b91b7fa4890
DeartF <i.artemon87@gmail.com>
2018-07-31 15:15:02 +0600

    myVue

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="training">
+    <h1>Math trainig</h1>
+    <hr>
+    <app-start-screen 
+      v-if = "state == 'start'" 
+      @onStart = "onStart">
+      
+    </app-start-screen>
+    <app-result-screen v-else-if = "state == 'results'"></app-result-screen>
+    <app-question v-else-if = "state == 'question'"></app-question>
+    <app-message v-else-if = "state == 'message'"></app-message>
+    <div v-else>Unknow state</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      state: 'start'
+    }
+  },
+  methods: {
+    onStart(){
+      this.state = 'question';
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .training {
+    max-width: 700px;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/components/Question.vue
@@ -0,0 +1,75 @@
+<template>
+	<div class="alert">
+		<h3>{{x}} + {{y}} = ?</h3>
+		<hr>
+		<div class="buttons">
+			<button class="btn btn-success" 
+					v-for = "number in answers"
+					@click = "onAnswer(number)">
+				{{number}}
+			</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				x: mtRand(100, 200),
+				y: mtRand(100, 200)
+			}
+		},
+		computed: {
+			good() {
+				return this.x + this.y;
+			},
+
+			answers(){
+				let res = [this.good];
+
+				while(res.length < 4) {
+					let num = mtRand(this.good -20 , this.good + 20)
+
+					if(res.indexOf(num) === -1){
+						res.push(num)
+					}
+				}
+
+				return res.sort(function() {
+					return Math.random() > 0.5; 
+				});
+			}
+		},
+		methods: {
+			onAnswer(num) {
+				if(num == this.good){
+					
+				} else {
+					
+				}
+			}
+		}
+	}
+
+	function mtRand(min, max) {
+		let diff = max-min;
+		return Math.floor(Math.random() * (diff + 1)) + min;
+	}
+</script>
+
+<style scoped>
+	.alert {
+		padding-top: 20px;
+		background-color: #eee;
+	}
+
+	.buttons {
+		display: -webkit-flex;
+		display: -moz-flex;
+		display: -ms-flex;
+		display: -o-flex;
+		display: flex;
+		justify-content: space-around;
+	}
+</style>
